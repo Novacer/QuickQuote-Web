d0441b3ab218c0503f280ef04587b6a7e9c57f21
Jackamac-attack <zhangjack27@gmail.com>
2018-02-20 20:23:56 -0500

    implemented registration feature, can now communicate w/ mongodb

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -14,12 +14,41 @@ export class RegisterComponent implements OnInit {
   username: String;
   email: String;
   password: String;
+  regFailed: boolean;
 
   constructor(private validateService: ValidateService, private authService: AuthService, private router: Router) {
-
+    this.regFailed = false;
   }
 
   ngOnInit() {
+  }
+
+  onRegSubmit() {
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password
+    };
+
+    if (this.validateService.validateRegister(user)) {
+      this.regFailed = false;
+
+      // register user
+
+      this.authService.registerUser(user).subscribe(data => {
+        if (data.success) {
+          this.regFailed = false;
+          this.router.navigateByUrl('/login');
+        }
+        else {
+          this.regFailed = true;
+        }
+      });
+    }
+    else {
+      this.regFailed = true;
+    }
   }
 
 }
