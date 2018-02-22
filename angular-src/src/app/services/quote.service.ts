c48a8b06d5c9018a726782a56e3003776cdd48d0
Jackamac-attack <zhangjack27@gmail.com>
2018-02-21 22:27:33 -0500

    implemented functions to approximate RCT

--- angular-src/src/app/services/quote.service.ts
+++ angular-src/src/app/services/quote.service.ts
@@ -1,8 +1,94 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Injectable()
+
 export class QuoteService {
 
-  constructor() { }
+  m_client: Object;
+  m_house: Object;
+  quote: Quote;
+  gore: Company;
+  intact: Company;
+  wawanesa: Company;
+  family: Company;
+
+  constructor(private authService: AuthService) { }
+
+  approxRCTWithBasement(sqft: number, custom: string) {
+    if (custom == "Standard") {
+      return sqft*200;
+    }
+    else if (custom == "Semi-custom") {
+      return sqft*275;
+    }
+    else if (custom == "Custom") {
+      return sqft*310;
+    }
+    else {
+      return -1;
+    }
+  }
+
+  approxRCTNoBasement(sqft: number, custom: string) {
+    if (custom == "Standard") {
+      return sqft*200;
+    }
+    else if (custom == "Semi-custom") {
+      return sqft*250;
+    }
+    else if (custom == "Custom") {
+      return sqft*300;
+    }
+    else {
+      return -1;
+    }
+  }
+
+  approxRCT1StoreyBase(sqft: number, custom: string) {
+    if (custom == "Standard") {
+      return sqft*200;
+    }
+    else if (custom == "Semi-custom") {
+      return sqft*225;
+    }
+    else if (custom == "Custom") {
+      return sqft*275;
+    }
+    else {
+      return -1;
+    }
+  }
+
+  approxRCT1Storey(sqft: number, custom: string) {
+    if (custom == "Standard") {
+      return sqft*180;
+    }
+    else if (custom == "Semi-custom") {
+      return sqft*210;
+    }
+    else if (custom == "Custom") {
+      return sqft*250;
+    }
+    else {
+      return -1;
+    }
+  }
+
+  createAndSaveQuote(client: Object, house: Object) {
+
+  }
+
+}
+
+interface Quote {
+  client_name: string;
+  companies: Company[];
+}
+
+interface Company {
+  company_name: string;
+  discountList: string[];
+  score: number;
 
 }
