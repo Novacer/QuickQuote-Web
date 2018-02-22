c75e1e96fc1ed14178beb96e93536e53a5a573f0
Jackamac-attack <zhangjack27@gmail.com>
2018-02-21 21:00:50 -0500

    ngModel can now access the information entered in the quote form:

--- angular-src/src/app/components/quote/quote.component.ts
+++ angular-src/src/app/components/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quote',
@@ -7,9 +9,122 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
-  constructor() { }
+  same_mailing: boolean;
+
+  client: Client;
+  client_name: string;
+  client_account_num: string;
+  client_dob: string;
+  client_claims_free: number;
+  client_comments: string;
+
+  client_street: string;
+  client_province: string;
+  client_postal_code: string;
+  client_mailing: Address;
+
+  house_street: string;
+  house_province: string;
+  house_postal_code: string;
+
+  house: House;
+  house_address: Address;
+  house_build: string;
+  house_year_built: number;
+  house_sqft: number;
+  house_basement_sqft: number;
+  house_num_floors: string;
+  house_updated: boolean;
+  house_alarm_type: string;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.same_mailing = true;
+    this.client_province = "BC";
+    this.house_province = "BC";
+    this.house_build = "Standard";
+    this.house_num_floors = "1";
+    this.house_updated = true;
+    this.house_alarm_type = "None";
+    this.client_comments = "";
+  }
 
   ngOnInit() {
   }
 
+  onQuoteClick() {
+    this.client_mailing = {
+      street: this.client_street,
+      province: this.client_province,
+      postal_code: this.client_postal_code
+    }
+
+    if (this.same_mailing) {
+      this.house_address = this.client_mailing;
+    }
+    else {
+      this.house_address = {
+        street: this.house_street,
+        province: this.house_province,
+        postal_code: this.house_postal_code
+      }
+    }
+
+    this.client = {
+      name: this.client_name,
+      account_num: this.client_account_num,
+      dob: this.client_dob,
+      claims_free: this.client_claims_free,
+      mailing: this.client_mailing,
+      comments: this.client_comments
+    }
+
+    this.house = {
+      address: this.house_address,
+      build_quality: this.house_build,
+      year_built: this.house_year_built,
+      sqft: this.house_sqft,
+      basement_sqft: this.house_basement_sqft,
+      num_floors: this.house_num_floors,
+      updated: this.house_updated,
+      alarm_type: this.house_alarm_type
+    }
+
+    console.log(this.client);
+    console.log(this.house);
+  }
+
+  changeUpdate(status: string) {
+    if (status == 'Less than 25 years old') {
+      this.house_updated = true;
+    }
+    else {
+      this.house_updated = false;
+    }
+  }
+}
+
+interface Client {
+  name: string;
+  account_num: string;
+  dob: string;
+  claims_free: number;
+  mailing: Address;
+  comments: string;
+}
+
+interface House {
+  address: Address;
+  build_quality: string;
+  year_built: number;
+  sqft: number;
+  basement_sqft: number;
+  num_floors: string;
+  updated: boolean;
+  alarm_type: string;
+}
+
+interface Address {
+  street: string;
+  province: string;
+  postal_code: string;
 }
